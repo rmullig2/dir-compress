5f933e27d25cc9d1e397e3377e974292e6822dfd
Ray Mulligan <rmullig2@optonline.net>
2018-08-16 21:07:38 -0400

    import os and sys, start function to check the arguments

--- dircompress.py
+++ dircompress.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-
+import os
 import argparse
+import sys
 
 def parse_arguments():
   parser = argparse.ArgumentParser(version="Version 1.0")
@@ -11,11 +12,20 @@ def parse_arguments():
                     help='Add this switch to show saved space without compressing')
   return parser
 
+def check_arguments(dir_name, email, file_size):
+  if not os.path.isdir(dir_name):
+    print "Invalid path name: " + dir_name
+    sys.exit(2)
+  else:
+    print "Good"
+  return true
+
 args = parse_arguments()
 #print args.parse_args()
 dir_name = args.parse_args().dir_name
 email = args.parse_args().email
 file_size = args.parse_args().file_size
 DRYRUN = args.parse_args().dry_run
+check_arguments(dir_name, email, file_size)
 # print dir_name + " | " + email + " | " + file_size + " | "
 # print DRYRUN
