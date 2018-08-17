5772ebf968b008f8babd0a84dfeae0e85584c901
Ray Mulligan <rmullig2@optonline.net>
2018-08-16 21:45:12 -0400

    Make the arguments required

--- dircompress.py
+++ dircompress.py
@@ -6,9 +6,9 @@ import re
 
 def parse_arguments():
   parser = argparse.ArgumentParser(version="Version 1.0")
-  parser.add_argument('-d', action='store', dest='dir_name', help='Specifies the directory for compression')
-  parser.add_argument('-e', action='store', dest='email', help='Specifies the email address')
-  parser.add_argument('-s', action='store', dest='file_size', help='Specifies the minimum file size to compress')
+  parser.add_argument('-d', action='store', dest='dir_name', help='Specifies the directory for compression', required=True)
+  parser.add_argument('-e', action='store', dest='email', help='Specifies the email address', required=True)
+  parser.add_argument('-s', action='store', dest='file_size', help='Specifies the minimum file size to compress', required=True)
   parser.add_argument('--dry-run', action='store_true', default=False, dest='dry_run',
                     help='Add this switch to show saved space without compressing')
   return parser
