821949bdf79c0e0185d2f276d2f4b23ea8380a62
Ray Mulligan <rmullig2@optonline.net>
2018-08-17 00:47:24 -0400

    Finish function to convert file size to an integer

--- dircompress.py
+++ dircompress.py
@@ -13,7 +13,7 @@ def parse_arguments():
   parser = argparse.ArgumentParser(version="Version 1.0")
   parser.add_argument('-d', action='store', dest='dir_name', help='Specifies the directory for compression', required=True)
   parser.add_argument('-e', action='store', dest='email', help='Specifies the email address', required=True)
-  parser.add_argument('-s', action='store', dest='file_size', help='Specifies the minimum file size to compress', required=True)
+  parser.add_argument('-s', action='store', dest='file_size', help='Specifies the minimum file size to compress: NUMBER[K|M|G]', required=True)
   parser.add_argument('--dry-run', action='store_true', default=False, dest='dry_run',
                     help='Add this switch to show saved space without compressing')
   return parser
@@ -35,6 +35,24 @@ def check_arguments(dir_name, email, file_size):
     sys.exit(2)
   return
 
+def convert_size(file_size):
+  """
+  This function will take the file size entered by the user and stored in a string.
+  It will determine if the value is in bytes, kilobytes, megabytes, or gigabytes.
+  Then it will calculate the file size in bytes and return an integer.
+  """
+  last_char = file_size[-1].upper()
+  if last_char == 'K':
+    multiplier = 1024
+  elif last_char == 'M':
+    multiplier = 1048576
+  elif last_char == 'G':
+    multiplier = 1073741824
+  else:
+    multiplier = 1
+  return int(re.match('[0-9]*', file_size).group(0)) * multiplier
+
+
 args = parse_arguments()
 #print args.parse_args()
 dir_name = args.parse_args().dir_name
@@ -44,3 +62,5 @@ DRYRUN = args.parse_args().dry_run
 check_arguments(dir_name, email, file_size)
 # print dir_name + " | " + email + " | " + file_size + " | "
 # print DRYRUN
+file_size = convert_size(file_size)
+#print file_size
